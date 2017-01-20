e1b4f05e7a87a28bb59b64b69c2b82c15fa8372b
OhmGeek <ryan@ohmgeek.co.uk>
2017-01-20 21:02:14 +0000

    Updated CIS auth to get user data, and only allow Aidan's users

--- jcrsite/cisauth/backends.py
+++ jcrsite/cisauth/backends.py
@@ -12,20 +12,26 @@ class CISBackend(object):
     def authenticate(self,username=None,password=None):
          # see if the user is valid on the durham server
         validator_resp = requests.get('https://www.dur.ac.uk/its/password/validator', auth=(username,password)) 
-        college_info = requests.get('https://community.dur.ac.uk/grey.jcr/itsuserdetailsjson.php', data={'username': username})
-        college = "";
+        college_info = requests.get('https://community.dur.ac.uk/grey.jcr/itsuserdetailsjson.php', params={'username': str(username)})
+        print(username)
+        print(college_info.json())
+        info = None
         # now determine whether the user is at Aidan's
         if(college_info.status_code != 400):
-            college = college_info.json()['college']
+            info = college_info.json()
 
-        if(validator_resp.status_code != 401):
-            # THE USER IS SUCCESS
+        if(validator_resp.status_code != 401 and info is not None and info['college']=="St Aidan's College"):
+            # The user is authenticated, and part of Aidan's
             try:
                 return User.objects.get(username=username)
             except User.DoesNotExist:
+                # Create a new Django user, getting information from the info database
                 user = User(username=username)
                 user.is_staff = False
                 user.is_superuser = False
+                user.email = info['email']
+                user.first_name = info['firstnames']
+                user.last_name = info['surname']
                 user.save()
                 return user
         return None
