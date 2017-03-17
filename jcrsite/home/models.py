f09002b23f9635357f89339a89706fecf09637d6
OhmGeek <ryan@ohmgeek.co.uk>
2017-03-17 16:03:51 +0000

    Created homepage models, carousel is a work in progress

--- jcrsite/home/models.py
+++ jcrsite/home/models.py
@@ -1,9 +1,40 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.wagtailcore.fields import RichTextField
 
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailsearch import index
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body')
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+        InlinePanel('gallery_images', label="Gallery Images"),  # the carousel on the page
+    ]
+
+
+class MainPageCarouselImage(Orderable):
+    page = ParentalKey(HomePage, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    title = models.CharField(blank=True, max_length=250)
+    description = models.CharField(blank=True, max_length=500)
+    primary = models.BooleanField(default=False)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('title'),
+        FieldPanel('description'),
+        FieldPanel('primary'),
+    ]
--- jcrsite/jcrsite/settings/base.py
+++ jcrsite/jcrsite/settings/base.py
@@ -145,3 +145,5 @@ WAGTAIL_SITE_NAME = "St Aidan's JCR"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://st-aidans.com'
+
+
