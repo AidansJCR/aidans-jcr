16a1b11607633d640d25ea935a89a976de8c818c
Ryan Collins <ryan@ohmgeek.co.uk>
2017-06-25 18:39:08 +0100

    Update error pages

--- jcrsite/settings/dev.py
+++ jcrsite/settings/dev.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'y3nc$5+yz$j-z6*b391t196qli7$vs4yqux=spn_#4h+8a3%ec'
