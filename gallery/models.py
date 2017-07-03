8b31dbf233a9a46732c6e02dfc249fdd50f8755e
Ryan Collins <ryan@ohmgeek.co.uk>
2017-07-03 14:37:10 +0100

    Added gallery

--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,28 @@
 from django.db import models
+from django.conf import settings
+from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, PageChooserPanel
+from wagtail.wagtailcore.fields import RichTextField
+
+from wagtail.wagtailcore.models import Page, Orderable
 
 # Create your models here.
 
 class AlbumPage(Page):
-    description = models.TextField()
-    
+    description = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel('description', classname="full"),
+        InlinePanel('gallery_images', label="Images"),  # the carousel on the page
+    ]
+class AlbumImage(Orderable):
+    page = ParentalKey(AlbumPage, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+
+    panels = [
+        ImageChooserPanel('image'),
+    ]
--- jcrsite/settings/base.py
+++ jcrsite/settings/base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'home',
     'search',
     'forms',
+    'gallery',
     'aidansadmintheme',
     'wagtail.wagtailforms',
     'wagtail.wagtailredirects',
