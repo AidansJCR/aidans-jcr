17cf66a14e0943389a1ce613ddda16e9c156b16b
Ryan Collins <ryan@ohmgeek.co.uk>
2017-07-04 16:32:18 +0100

    Add s3 details

--- jcrsite/settings/base.py
+++ jcrsite/settings/base.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'modelcluster',
     'taggit',
     'wagtailmenus',
-
+    'storages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -159,3 +159,9 @@ BASE_URL = 'http://st-aidans.com'
 db_from_env = dj_database_url.config(conn_max_age=500)
 if db_from_env:
     DATABASES['default'].update(db_from_env)
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
