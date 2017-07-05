0746bcf7bb21917ed4290010b84b770e38ba927c
Ryan Collins <ryan@ohmgeek.co.uk>
2017-07-05 14:41:46 +0100

    Added SendGrid for emailing

--- jcrsite/settings/base.py
+++ jcrsite/settings/base.py
@@ -159,9 +159,13 @@ BASE_URL = 'http://st-aidans.com'
 db_from_env = dj_database_url.config(conn_max_age=500)
 if db_from_env:
     DATABASES['default'].update(db_from_env)
+    # AWS
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # SendGrid
+    EMAIL_BACKEND = "sgbackend.SendGridBackend"
+    SENDGRID_API_KEY = os.environ['SENDGRID_API_KEY']
