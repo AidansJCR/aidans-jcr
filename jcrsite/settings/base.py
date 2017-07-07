bbaeee288b377657f0c49ba8a56b55ba4c5ab979
Ryan Collins <ryan@ohmgeek.co.uk>
2017-07-07 17:23:52 +0100

    Add S3 Caching

--- jcrsite/settings/base.py
+++ jcrsite/settings/base.py
@@ -166,6 +166,9 @@ if db_from_env:
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
     # SendGrid
     EMAIL_BACKEND = "sgbackend.SendGridBackend"
     SENDGRID_API_KEY = os.environ['SENDGRID_API_KEY']
