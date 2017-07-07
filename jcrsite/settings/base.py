591acd986e9dc3c42dd61b40ac0e50acd2e8886e
Ryan Collins <ryan@ohmgeek.co.uk>
2017-07-07 17:46:12 +0100

    Secret key in env variables

--- jcrsite/settings/base.py
+++ jcrsite/settings/base.py
@@ -176,3 +176,9 @@ if db_from_env:
     # SendGrid
     EMAIL_BACKEND = "sgbackend.SendGridBackend"
     SENDGRID_API_KEY = os.environ['SENDGRID_API_KEY']
+
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+else:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'y3nc$5+yz$j-z6*b391t196qli7$vs4yqux=spn_#4h+8a3%ec'
