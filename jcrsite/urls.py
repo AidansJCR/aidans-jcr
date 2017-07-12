5154ef406f48d64f6380baa8e4a5c78e10828735
Ryan Collins <ryan@ohmgeek.co.uk>
2017-07-12 11:06:52 +0100

    Add sitemaps

--- jcrsite/urls.py
+++ jcrsite/urls.py
@@ -8,15 +8,14 @@ from search import views as search_views
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
+from wagtail.contrib.wagtailsitemaps.views import sitemap
 
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
-
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
-
     url(r'^search/$', search_views.search, name='search'),
-
+    url(r'^sitemap\.xml$', sitemap),
     url(r'', include(wagtail_urls)),
 ]
 
