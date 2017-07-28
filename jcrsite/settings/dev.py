25d23bdd618978349ffc7b92003d08cc05f78f38
Ryan Collins <ryan@ohmgeek.co.uk>
2017-07-28 15:06:40 +0100

    Hopefully use correct settings this time

--- jcrsite/settings/dev.py
+++ jcrsite/settings/dev.py
@@ -3,7 +3,11 @@ from __future__ import absolute_import, unicode_literals
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'y3nc$5+yz$j-z6*b391t196qli7$vs4yqux=spn_#4h+8a3%ec'
+
 
 try:
     from .local import *
--- jcrsite/settings/production.py
+++ jcrsite/settings/production.py
@@ -1,8 +1,45 @@
 from __future__ import absolute_import, unicode_literals
 
 from .base import *
+import dj_database_url
+import os
 
 DEBUG = False
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+if db_from_env:
+    DATABASES['default'].update(db_from_env)
+    # AWS
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    AWS_S3_BUCKET_AUTH = False
+
+    AWS_S3_MAX_AGE_SECONDS = 60 * 60 * 4  # 4 hours
+    AWS_QUERYSTRING_AUTH = False
+    AWS_HEADERS = {
+        'Cache-Control': 'max-age=86400',
+    }
+    # SendGrid
+    EMAIL_BACKEND = "sgbackend.SendGridBackend"
+    SENDGRID_API_KEY = os.environ['SENDGRID_API_KEY']
+
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+    # SSL
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = True
+
+    # Static file compression and caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 try:
     from .local import *
