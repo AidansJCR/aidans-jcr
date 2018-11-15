e57eab9fb7b0ebfd174d7dba81c0563e87060be2
CADuffy <connor.a.duffy98@gmail.com>
2018-11-15 19:18:29 +0000

    Removed old function that may be breaking everything :)

--- jcrsite/templatetags/navbar.py
+++ jcrsite/templatetags/navbar.py
@@ -7,11 +7,11 @@ from django.conf import settings
 register = template.Library()
 
 
-@register.simple_tag(takes_context=True)
+'''@register.simple_tag(takes_context=True)
 def get_site_root(context):
     # NB this returns a core.Page, not the implementation-specific model used
     # so object-comparison to self will return false as objects would differ
-    return context['request'].site.root_page
+    return context['request'].site.root_page'''
 
 
 def has_menu_children(page):
