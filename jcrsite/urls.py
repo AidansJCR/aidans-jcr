65b812779c060531b171f6f1edbf26bc83b01488
CADuffy <connor.a.duffy98@gmail.com>
2018-11-27 16:20:53 +0000

    Fixed login screen after major update

--- jcrsite/urls.py
+++ jcrsite/urls.py
@@ -13,8 +13,8 @@ from wagtail.contrib.sitemaps.views import sitemap
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', auth_views.LoginView, name='login'),
-    url(r'^logout/$', auth_views.LogoutView, name='logout'),
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^django-admin/', admin.site.urls),
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
