6f913fd1d8ba57792828b8366667bd187b3b4e62
Connor <connor.a.duffy98@gmail.com>
2019-06-05 12:52:12 +0100

    More app updates

--- home/models.py
+++ home/models.py
@@ -246,6 +246,4 @@ class AppAnnouncement(models.Model):
     title = models.CharField(max_length=200)
     message = models.TextField()
     time_set = models.TimeField()
-    image = models.ForeignKey(
-        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
-    )
+    image = models.ImageField()
