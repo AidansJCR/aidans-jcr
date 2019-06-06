b285a661c98677855a59967da4641d267c6520ee
Connor <connor.a.duffy98@gmail.com>
2019-06-06 22:18:13 +0100

    The announcements can now be retrieved from /getannouncements

--- home/views.py
+++ home/views.py
@@ -51,7 +51,7 @@ def app_announcements(request):
         elif request.POST['function'] == 'delete':
             #If the user is removing an announcement
             AppAnnouncement.objects.get(pk=request.POST['announcementId']).delete()
-        return redirect('home/app/announcements')
+        return redirect('/app/announcements')
     elif request.method == 'GET':
         announcements = AppAnnouncement.objects.all()
         return render(request, 'home/app/announcements.html', {'announcements':announcements})
@@ -60,4 +60,5 @@ def app_announcements(request):
 
 def app_get_announcements(request):
     #Send back the announcements in a json format
-    return JsonResponse([{'title':'Hello world!'}], safe=False)
+    announcements = list(AppAnnouncement.objects.all().values())
+    return JsonResponse(announcements, safe=False)
