9b33782887bf685acbe5f8c14a10c8a3d6e2baf4
Connor Duffy <caduffy98@gmail.com>
2020-01-11 23:21:09 +0000

    Package updates and url tweaks

--- home/urls.py
+++ home/urls.py
@@ -15,13 +15,15 @@ from home import views as home_views
 
 urlpatterns = [
     #The api url patterns
-    path('login', home_views.app_login),
-    path('logout', home_views.app_logout),
-    path('announcements', home_views.app_announcements),
-    path('getannouncements', home_views.app_get_announcements),
-    path('schedule', home_views.app_schedule),
-    path('getschedule', home_views.app_get_schedule),
-    path('', home_views.app_home_page),
+    path('login', home_views.login),
+    path('logout', home_views.logout),
+    path('announcements', home_views.announcements),
+    path('getannouncements', home_views.get_announcements),
+    path('schedule', home_views.schedule),
+    path('getschedule', home_views.get_schedule),
+    path('chat', home_views.schedule),
+    path('booking', home_views.schedule),
+    path('', home_views.home_page),
 ]
 
 
--- home/views.py
+++ home/views.py
@@ -9,15 +9,15 @@ from datetime import datetime
 import json
 
 
-@login_required(login_url='/app/login')
-def app_home_page(request):
-    return render(request, 'home/app/home_page.html')
+@login_required(login_url='/user/login')
+def home_page(request):
+    return render(request, 'home/user/home.html')
 
 
-def app_login(request):
+def login(request):
     if request.method == 'GET':
         #They are requesting the login page
-        return render(request, 'home/app/login.html', {'error':''})
+        return render(request, 'home/user/login.html', {'error':''})
     elif request.method == 'POST':
         #They are sending us some data to try and login
         username = request.POST['username']
@@ -25,21 +25,21 @@ def app_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('/app')
+            return redirect('/user')
         else:
-            return render(request, 'home/app/login.html', {'error':'Incorrect login'})
+            return render(request, 'home/user/login.html', {'error':'Incorrect login'})
     else:
         #They are trying to do something weird
         return None
 
 
-def app_logout(request):
+def logout(request):
     logout(request)
-    return redirect('/app/login')
+    return redirect('/user/login')
 
 
-@login_required(login_url='/app/login')
-def app_announcements(request):
+@login_required(login_url='/user/login')
+def announcements(request):
     if request.method == 'POST':
         if request.POST['function'] == 'upload':
             #If the user is uploading an announcement
@@ -48,22 +48,22 @@ def app_announcements(request):
         elif request.POST['function'] == 'delete':
             #If the user is removing an announcement
             AppAnnouncement.objects.get(pk=request.POST['announcementId']).delete()
-        return redirect('/app/announcements')
+        return redirect('/user/announcements')
     elif request.method == 'GET':
         announcements = AppAnnouncement.objects.all()
-        return render(request, 'home/app/announcements.html', {'announcements':announcements})
+        return render(request, 'home/user/announcements.html', {'announcements':announcements})
     return None
 
 
-def app_get_announcements(request):
+def get_announcements(request):
     if request.method == 'GET':
         #Send back the announcements in a json format
         announcements = list(AppAnnouncement.objects.order_by('time_set').values())
         return JsonResponse(announcements, safe=False)
 
 
-@login_required(login_url='/app/login')
-def app_schedule(request):
+@login_required(login_url='/user/login')
+def schedule(request):
     if request.method == 'POST':
         if request.POST['function'] == 'upload':
             #If the user is uploading an announcement
@@ -72,14 +72,14 @@ def app_schedule(request):
         elif request.POST['function'] == 'delete':
             #If the user is removing an announcement
             AppEvent.objects.get(pk=request.POST['eventId']).delete()
-        return redirect('home/app/schedule')
+        return redirect('home/user/schedule')
     elif request.method == 'GET':
         events = AppEvent.objects.all()
-        return render(request, 'home/app/schedule.html', {'events':events})
+        return render(request, 'home/user/schedule.html', {'events':events})
     return None
 
 
-def app_get_schedule(request):
+def get_schedule(request):
     if request.method == 'GET':
         events = list(AppEvent.objects.order_by('start_time').values())
         return JsonResponse(events, safe=False)
