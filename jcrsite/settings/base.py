e829f0f3937df1b8609538e348b09667e21a7605
AidansIT <30476659+AidansIT@users.noreply.github.com>
2022-10-21 20:50:02 +0100

    remove redis from base

--- jcrsite/settings/base.py
+++ jcrsite/settings/base.py
@@ -116,14 +116,14 @@ WSGI_APPLICATION = 'jcrsite.wsgi.application'
 # Routing for channel sockets
 #ASGI_APPLICATION = "jcrsite.routing.application"
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
-}
+#CHANNEL_LAYERS = {
+#    'default': {
+#        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#        'CONFIG': {
+#            "hosts": [('127.0.0.1', 6379)],
+#        },
+#    },
+#}
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
